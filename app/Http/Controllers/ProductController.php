eb7a1d21174ba99c42cdb569b7bffe81642e0175
Denis Capelete <capelete.denis@gmail.com>
2025-11-17 19:52:27 -0300

    Validando Query Params

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    public function getAllProducts()
+    public function getAllProducts(Request $request)
     {
+        $validator = Validator::make($request->query(), [
+            'metadata' => ['sometimes', 'string'],
+            'orderBy' => ['sometimes', 'in:views,selling,price'],
+            'limit' => ['sometimes', 'numeric'],
+        ], [
+            'metadata' => 'O metadata deve ser um JSON válido',
+            'orderBy' => 'O campo orderBy deve ser um dos seguintes:views, selling ou price',
+            'limit' => "O limite deve ser um número válido"
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+                'products' => []
+            ], 400);
+        }
         $products = Product::with('images')->get();
         return response()->json([
             'error' => null,
